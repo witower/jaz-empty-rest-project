eeb90311ac0f9877fc210e070577c31f6b0fd5c3
witower <s16125@pjwstk.edu.pl>
2018-05-11 23:14:10 +0200

    Final

---
+++ z-domainstart-deploy.bat
@@ -0,0 +1,3 @@
+call cd C:\Users\wit\Workspace\PJATK\4-JAZ\JAZ_z4_empty-rest-project
+call asadmin start-domain
+call asadmin deploy target/samplerestapp.war
---
+++ zzz-undeploy-domainstop.bat
@@ -0,0 +1,2 @@
+call asadmin undeploy samplerestapp
+call asadmin stop-domain
